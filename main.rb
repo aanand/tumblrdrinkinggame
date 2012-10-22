fd2d7afe320aab5e44d1c08c4acc7e39567f2d00
Aanand Prasad <aanand.prasad@gmail.com>
2012-10-22 17:21:36 +0100

    Redirect Heroku URLs to proper domain

--- main.rb
+++ main.rb
@@ -5,6 +5,10 @@ require 'json'
 require 'net/http'
 require './views/bourbon/lib/bourbon'
 
+before do
+  redirect "http://www.tumblrdrinkinggame.com#{request.path}" if request.host =~ /heroku/
+end
+
 get '/' do
   headers['Cache-Control'] = 'public; max-age=300'
   haml :index
